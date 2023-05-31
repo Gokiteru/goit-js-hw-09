81ed75609e7b9e8076e781eeb2eaa18cee769401
Gokiteru <vakin.vlad@gmail.com>
2023-05-31 14:11:23 +0300

    1st task done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,23 @@
+var startButton = document.querySelector(".data-start");
+var stopButton = document.querySelector(".data-stop");
+var intervalId;
 
+startButton.addEventListener('click', function () {
+  startButton.disabled = true;
+  stopButton.disabled = false;
+  intervalId = setInterval(function () {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, 100);
+});
+
+stopButton.addEventListener('click', function () {
+  startButton.disabled = false;
+  stopButton.disabled = true;
+  clearInterval(intervalId);
+});
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215)
+      .toString(16)
+      .padStart(6, '0')}`;
+  }
